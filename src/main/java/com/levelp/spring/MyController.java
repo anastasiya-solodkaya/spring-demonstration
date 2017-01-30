1b2279039064dc7bb32109e48b2dfc5256f181c8
anastasiya-solodkaya <a.s.solodkaya@gmail.com>
2017-01-30 03:03:46 +0300

    Видео 2 - подключаем hibernate

--- src/main/java/com/levelp/spring/MyController.java
+++ src/main/java/com/levelp/spring/MyController.java
@@ -1,6 +1,8 @@
 package com.levelp.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -8,8 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class MyController {
+
+    final SimpleDBAccess access;
+
+    @Autowired
+    public MyController(SimpleDBAccess access) {
+        this.access = access;
+    }
+
     @RequestMapping(value = "/hello")
-    public String hello(){
+    public String hello(ModelMap map){
+        map.addAttribute("db", access.getVersion());
         return "index";
     }
 }
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Hello, ${db}!</h2>
 </body>
 </html>
